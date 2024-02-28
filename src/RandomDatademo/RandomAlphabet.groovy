cc7b7fae0f4071529ac63928a2bf641d7bad88aa
vpatil <vivekpatil2291997@gmail.com>
2024-02-28 16:35:58 +0530

    Committed on 28-02-2024 for groovy

--- src/RandomDatademo/RandomAlphabet.groovy
+++ src/RandomDatademo/RandomAlphabet.groovy
@@ -1,4 +1,13 @@
 package RandomDatademo
 
 class RandomAlphabet {
+
+    def getRandomCountry() {
+        // List of countries
+        List<String> countries = ['USA', 'Canada', 'UK', 'Australia', 'Germany', 'France', 'Japan', 'Brazil', 'India', 'China']
+
+        // Randomly select a country
+        String randomCountry = countries[new Random().nextInt(countries.size())]
+        return randomCountry
+    }
 }
