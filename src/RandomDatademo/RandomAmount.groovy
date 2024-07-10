7212e40ef97e42111b640215ee6c80098cfd2def
vpatil <vivekpatil2291997@gmail.com>
2024-07-10 21:01:04 +0530

    Committed on 10-07-2024 for groovy

--- src/RandomDatademo/RandomAmount.groovy
+++ src/RandomDatademo/RandomAmount.groovy
@@ -7,9 +7,37 @@ class RandomAmount {
         return 100 + rand.nextInt(90000) // Generates a random number between 1000 and 9999 (inclusive)
     }
 
-    @Keyword
+    //@Keyword
     def randomHours() {
         String number = new Random().nextInt(24 - 1 + 1) + 1
         return number
     }
+
+    //@Keyword
+    def generateRandomLink() {
+        def baseUrl = "https://www."
+        def domains = [
+                "google",
+                "yahoo",
+                "bing",
+                "duckduckgo",
+                "baidu",
+                "yandex"
+        ]
+        def tlds = [
+                ".com",
+                ".org",
+                ".net",
+                ".info",
+                ".biz"
+        ]
+
+        def random = new Random()
+        def randomDomain = domains[random.nextInt(domains.size())]
+        def randomTld = tlds[random.nextInt(tlds.size())]
+
+        return baseUrl + randomDomain + randomTld
+    }
+
+
 }
