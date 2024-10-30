3be6fd09d89e8ec280590df6a1cb0c90cee8d118
vpatil <vivekpatil2291997@gmail.com>
2024-10-31 03:18:28 +0530

    Committed on 31-10-2024 for groovy

---
+++ src/RandomDatademo/RandomCountryName.groovy
@@ -0,0 +1,28 @@
+package RandomDatademo
+
+class RandomCountryName {
+
+    Random rand = new Random()
+
+    // List of sample country names
+    List<String> countries = [
+            "United States", "Canada", "Mexico", "Brazil", "United Kingdom",
+            "France", "Germany", "Italy", "Australia", "India",
+            "China", "Japan", "South Korea", "Russia", "South Africa"
+    ]
+
+    // Method to get a random country name
+    String randomCountry() {
+        return countries[rand.nextInt(countries.size())]
+    }
+
+    static void main(String[] args) {
+        // Create an instance of RandomCountryName
+        RandomCountryName randomData = new RandomCountryName()
+
+        // Generate a random country name
+        String randomCountryName = randomData.randomCountry()
+
+        println("Random Country Name: " + randomCountryName)
+    }
+}
