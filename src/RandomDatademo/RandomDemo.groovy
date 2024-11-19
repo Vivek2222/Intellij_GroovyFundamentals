1e9faa29ea369d71154922df4b260e27eee31f9e
vpatil <vivekpatil2291997@gmail.com>
2024-11-19 10:08:24 +0530

    Committed on 19-11-2024 for groovy updated code format

--- src/RandomDatademo/RandomDemo.groovy
+++ src/RandomDatademo/RandomDemo.groovy
@@ -2,9 +2,11 @@ package RandomDatademo
 
 class RandomNumber {
     Random rand = new Random()
+
     long number() {
         return rand.nextInt(1000000000)
     }
+
     static void main(String[] args) {
         RandomNumber randomData = new RandomNumber()
         def randomNumber = randomData.number()
