88f4951c379d7dafd6a64d2fb7d8d69eb3bfc85e
vpatil <vivekpatil2291997@gmail.com>
2024-11-28 22:29:22 +0530

    Committed on 28-11-2024 for groovy updated code format

--- src/RandomDatademo/RandomPhonenumber.groovy
+++ src/RandomDatademo/RandomPhonenumber.groovy
@@ -1,18 +1,19 @@
 package RandomDatademo
 
 class RandomPhonenumber {
-        Random rand = new Random()
+    Random rand = new Random()
 
-        String mobileNumber() {
-            String mobile = '9' // Set the first digit to 9 to avoid starting with zero
-            for (int i = 1; i < 10; i++) {
-                mobile += rand.nextInt(10) // Generate random digits for the remaining 9 digits
-            }
-            return mobile
+    String mobileNumber() {
+        String mobile = '9' // Set the first digit to 9 to avoid starting with zero
+        for (int i = 1; i < 10; i++) {
+            mobile += rand.nextInt(10) // Generate random digits for the remaining 9 digits
         }
-        static void main(String[] args) {
-            RandomPhonenumber randomData = new RandomPhonenumber()
-            String randomMobileNumber = randomData.mobileNumber()
-            println(randomMobileNumber)
-        }
+        return mobile
     }
+
+    static void main(String[] args) {
+        RandomPhonenumber randomData = new RandomPhonenumber()
+        String randomMobileNumber = randomData.mobileNumber()
+        println(randomMobileNumber)
+    }
+}
